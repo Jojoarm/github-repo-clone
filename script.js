9bebd5d75b880f73f0c0732ee8750730acfe6001
George Ofogba <ofogbageorge3@gmail.com>
2021-05-23 08:34:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const data = {
+    "token" : "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",
+    "username" : "Jojoarm"
+}
+
+// const fetch = requirejs(["node-fetch"]);
+
+const query_repo = {
+  query: `
+        query { 
+            user(login: "Jojoarm") { 
+            name,
+            avatarUrl,
+                    login,
+                    bio,
+            repositories(first: 20, orderBy: {field:NAME, direction:ASC}) {
+            pageInfo {hasNextPage, endCursor}
+                totalCount,
+            nodes{
+                ... on Repository{
+                    id
+                        name,
+                updatedAt, 
+                forkCount,
+                stargazerCount,
+                        createdAt,
+                        url,
+                        description,
+                        isFork,
+                        languages(first:1){
+                        nodes{
+                            name
+                        }
+                        }
+                }
+                }
+                }
+            }
+        }
+	`,
+};
+
+const baseUrl = 'https://api.github.com/graphql'
+
+const headers = {
+    'Content-Type': 'application/json',
+    Authorization: 'bearer '+data.token
+}
+
+fetch(baseUrl, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(query_repo)
+}).
+then(res => res.json())
+.then(data => {
+    console.log(data.data)
+})
